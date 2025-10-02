1ea62cd0e246c1e8dde4c0a0eb7c9edfca90070b
8421BCED <0danielraj0@gmail.com>
2025-10-03 00:30:57 +0530

    your message

--- static/map.js
+++ static/map.js
@@ -1,23 +1,5 @@
- const styles = [
-    { name: "OpenCycleMap", url: `https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=${window.THUNDERFOREST_API_KEY}` },
-    // ... other styles
-];
-
-let map, layer;
-function initMap() {
-    map = L.map('map', { center: [20, 0], zoom: 2, ... });
-    layer = L.tileLayer(styles[0].url, { maxZoom: 22 }).addTo(map);
-}
-
-function changeStyle(index) {
-    map.removeLayer(layer);
-    layer = L.tileLayer(styles[index].url, { maxZoom: 22 }).addTo(map);
-}
-
-function searchLocation(query) {
-    fetch(`https://nominatim.openstreetmap.org/search?q=${query}&format=json`)
-        .then(response => response.json())
-        .then(data => {
-            // Handle search results
-        });
-}
+var map = L.map('map').setView([51.505, -0.09], 13); // London, zoom 13
+var tileLayer = L.tileLayer(`https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=${window.THUNDERFOREST_API_KEY}`, {
+    attribution: '&copy; <a href="https://www.thunderforest.com">Thunderforest</a>',
+    maxZoom: 19
+}).addTo(map);
