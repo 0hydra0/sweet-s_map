91011a93065b855c91aecf836ac3599c220d8083
8421BCED <0danielraj0@gmail.com>
2025-10-03 21:24:26 +0530

    your message

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
@@ -10,5 +11,5 @@ def index():
 def map_view():
     return render_template('layout.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Vercel serverless WSGI handler
+app = app
